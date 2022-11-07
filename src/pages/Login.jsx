494b4d68e705ff8ac6be5ac62acff2c3109f2bab
Truong Tin <buitruongtin@gmail.com>
2022-11-07 10:51:28 +0700

    change

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,12 +6,35 @@ export default function Login() {
         var uiConfig = {
             callbacks: {
                 signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+                    var user = authResult.user;
+                    var credential = authResult.credential;
+                    var isNewUser = authResult.additionalUserInfo.isNewUser;
+                    var providerId = authResult.additionalUserInfo.providerId;
+                    var operationType = authResult.operationType;
+                    // Do something with the returned AuthResult.
+                    // Return type determines whether we continue the redirect
+                    // automatically or whether we leave that to developer to handle.
                     return true;
                 },
+                signInFailure: function (error) {
+                    // Some unrecoverable error occurred during sign-in.
+                    // Return a promise when error handling is completed and FirebaseUI
+                    // will reset, clearing any UI. This commonly occurs for error code
+                    // 'firebaseui/anonymous-upgrade-merge-conflict' when merge conflict
+                    // occurs. Check below for more details on this.
+                    return handleUIError(error);
+                },
                 uiShown: function () {
+                    // The widget is rendered.
+                    // Hide the loader.
                     document.getElementById('loader').style.display = 'none';
                 }
             },
+            credentialHelper: firebaseui.auth.CredentialHelper.NONE,
+            // Query parameter name for mode.
+            queryParameterForWidgetMode: 'mode',
+            // Query parameter name for sign in success url.
+            queryParameterForSignInSuccessUrl: 'signInSuccessUrl',
             // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
             signInFlow: 'popup',
             signInSuccessUrl: '<url-to-redirect-to-on-success>',
@@ -44,7 +67,6 @@ export default function Login() {
                         auth_type: 'reauthenticate'
                     }
                 },
-                firebase.auth.TwitterAuthProvider.PROVIDER_ID, // Twitter does not support scopes.
                 {
                     provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
                     providerName: 'Email',
@@ -75,11 +97,29 @@ export default function Login() {
                     providerName: 'Github',
                     fullLabel: 'Sign in with ',
                     
+                },
+                {
+                    provider: firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID,
+                    providerName: 'Anonymous',
+                    fullLabel: 'Sign in with ',
                 }
             ],
+            // Set to true if you only have a single federated provider like
+            // firebase.auth.GoogleAuthProvider.PROVIDER_ID and you would like to
+            // immediately redirect to the provider's site instead of showing a
+            // 'Sign in with Provider' button first. In order for this to take
+            // effect, the signInFlow option must also be set to 'redirect'.
+            immediateFederatedRedirect: false,
+            // tosUrl and privacyPolicyUrl accept either url string or a callback
+            // function.
+            // Terms of service url/callback.
             tosUrl: '<your-tos-url>',
-            privacyPolicyUrl: '<your-privacy-policy-url>'
+            // Privacy policy url/callback.
+            privacyPolicyUrl: function () {
+                window.location.assign('<your-privacy-policy-url>');
+            }
         };
+        // The start method will wait until the DOM is loaded.
         ui.start('#firebaseui-auth-container', uiConfig);
     }, []);
     return (
