3c16ae11a54bbb1f12d9cf8a00126d013929aca2
Truong Tin <buitruongtin@gmail.com>
2022-11-10 13:54:25 +0700

    nav top func

---
+++ src/components/Nav/NavTop.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useRef, useState } from 'react'
+import { useNavigate, useLocation } from 'react-router-dom';
+
+export default function NavTop() {
+    const [location, setLocation] = useState('');
+    const btnBack = useRef();
+    const btnforward = useRef();
+    const lastLocation = useRef('');
+    let navigate = useNavigate();
+    let path = useLocation();
+    let pathname = path.pathname.split("/");
+    
+    useEffect(() => {
+        setLocation(pathname[1]);
+        lastLocation.current = location;
+        console.log('s', [pathname[1], location, lastLocation.current])
+        if(!lastLocation.current) {
+            btnforward.current = "not-allowed"
+        }
+        if(!location) {
+            btnBack.current = "not-allowed"
+        }
+    }, [pathname[1]]);
+
+    function backHistory() {
+        if(location !== '') {
+            navigate(-1);
+        }
+    }
+    function forward() {    
+        if(lastLocation.current)
+            navigate('/' + lastLocation.current);
+    }
+    return (
+        <>
+            <header className="">
+                <div className="">
+                    <button className="top-bar-back-button" onClick={() => backHistory()} style={{cursor: btnBack}}>
+                        <svg role="img" height="24" width="24" className="" viewBox="0 0 24 24">
+                            <path d="M15.957 2.793a1 1 0 010 1.414L8.164 12l7.793 7.793a1 1 0 11-1.414 1.414L5.336 12l9.207-9.207a1 1 0 011.414 0z"></path>
+                        </svg>
+                    </button>
+                    <button className="top-bar-forward-button" onClick={() => forward()} style={{cursor: btnforward}}>
+                        <svg role="img" height="24" width="24" className="" viewBox="0 0 24 24">
+                            <path d="M8.043 2.793a1 1 0 000 1.414L15.836 12l-7.793 7.793a1 1 0 101.414 1.414L18.664 12 9.457 2.793a1 1 0 00-1.414 0z"></path>
+                        </svg>
+                    </button>
+                </div>
+                <button className="" type="button">
+                    <span dir="auto" className="">Tín Trương</span>
+                    <svg role="img" height="16" width="16" className="" aria-hidden="true" viewBox="0 0 16 16">
+                        <path d="M14 6l-6 6-6-6h12z"></path>
+                    </svg>
+                </button>
+            </header>
+        </>
+    )
+}
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -2,6 +2,7 @@ import React, { useState, Suspense } from "react";
 import { Outlet } from "react-router-dom";
 const Player = React.lazy(() => import('../components/Player/Player'));
 const NavLeft = React.lazy(() => import('../components/Nav/NavLeft'));
+const NavTop = React.lazy(() => import('../components/Nav/NavTop'));
 import useWindowDimensions from '../components/Player/UseWindowDimensions';
 
 export default function Layout() {
@@ -27,33 +28,13 @@ export default function Layout() {
       <div className="top-container">
         <Suspense fallback={<div>Loading...</div>}>
           <div className="top-bar">
-            <header className="">
-              <div className="">
-                <button data-testid="top-bar-back-button" aria-label="Go back" className="">
-                  <svg role="img" height="24" width="24" className="" viewBox="0 0 24 24">
-                    <path d="M15.957 2.793a1 1 0 010 1.414L8.164 12l7.793 7.793a1 1 0 11-1.414 1.414L5.336 12l9.207-9.207a1 1 0 011.414 0z"></path>
-                  </svg>
-                </button>
-                <button aria-label="Go forward" disabled="" className="">
-                  <svg role="img" height="24" width="24" className="" viewBox="0 0 24 24">
-                    <path d="M8.043 2.793a1 1 0 000 1.414L15.836 12l-7.793 7.793a1 1 0 101.414 1.414L18.664 12 9.457 2.793a1 1 0 00-1.414 0z"></path>
-                  </svg>
-                </button>
-              </div>
-              <button className="" type="button">
-                <span dir="auto" className="">Tín Trương</span>
-                <svg role="img" height="16" width="16" className="" aria-hidden="true" viewBox="0 0 16 16">
-                  <path d="M14 6l-6 6-6-6h12z"></path>
-                </svg>
-              </button>
-            </header>
+            <NavTop />
           </div>
           <div className="nav-bar-left">
             <NavLeft />
           </div>
           <div className="main-view" style={{ height: height - 90 }}>
             <div className="main-view-container">
-
               <Outlet context={[items, setItems]} />
             </div>
           </div>
